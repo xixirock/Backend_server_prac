df5d188389cefa3bd43c2d85f0060725e48d9327
Jason Lin <jlin0402@gmail.com>
2017-12-16 10:43:49 -0800

    seperated files into sub directory

---
+++ data/data.js
@@ -0,0 +1,51 @@
+// Phase II: For this second phase, aim to complete the following:
+
+//     Backend Team:
+
+//     Create a basic Express server.
+
+// Your server at this point should do the BARE MINIMUM.(Effectively, it should just say: “Listening at PORT 3000” when the command node server.js is run.)
+
+// =====================================================================================
+
+// Phase III: For this third phase, aim to complete the following:
+
+//Backend Team:
+
+//Create a set of variables(hint: arrays of objects) for holding the reservation and waitlist data
+
+// Create a set of routes that then display this data as JSONs.Users should be given these JSONs
+// if they visit the appropriate page(i.e.if a user visits localhost: 3000 / api / tables they should see a JSON of table data). (RECOMMENDED TO USE POSTMAN FOR TESTING/DEBUGGING)
+
+// =====================================================================================
+
+// Phase IV: For this fourth phase, aim to complete the following:
+
+//     Backend Team:
+
+//     Create the logic that handles reservation requests.Your code should work such that POST requests take in JSON objects, checks
+// if there is any space left, then adds the JSON object to either the reservation array or the waitlist array.Your POST route should also respond to requests with a confirmation(true or false) of whether or not the requestor has a reservation(or is on the waiting list).
+
+// You should be using Postman to do all your testing at this point.
+
+// =====================================================================================
+
+// Phase V: For the fifth and final phase, aim to complete the following:
+
+//     All:
+
+//     Complete any remaining functionality from the previous phase
+// if you need more time.
+
+// Then, thoroughly test your application
+// for bugs.Check
+// if there are any obvious ways to crash the application.
+
+// Then,
+// if you have more time--begin to tackle any bonus of your choice.
+
+// =====================================================================================
+
+// SETUP
+// =====================================================================================
+// dependencies
